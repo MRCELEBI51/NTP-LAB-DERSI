c20ca85cb2bd461ac640b3bb3603abdbe6524f5f
Muhammet Ali Çelebi <147302807+MRCELEBI51@users.noreply.github.com>
2024-11-06 11:28:57 +0300

    Add files via upload

---
+++ TikTakToe/TikTakToe/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace TikTakToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(61, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(125, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = " ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.XOClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Location = new System.Drawing.Point(207, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = " ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.XOClick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.Location = new System.Drawing.Point(289, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = " ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.XOClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Silver;
+            this.button4.Location = new System.Drawing.Point(125, 242);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 56);
+            this.button4.TabIndex = 4;
+            this.button4.Text = " ";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.XOClick);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Silver;
+            this.button5.Location = new System.Drawing.Point(207, 242);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(76, 56);
+            this.button5.TabIndex = 5;
+            this.button5.Text = " ";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.XOClick);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Silver;
+            this.button6.Location = new System.Drawing.Point(289, 242);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(76, 56);
+            this.button6.TabIndex = 6;
+            this.button6.Text = " ";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.XOClick);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Silver;
+            this.button7.Location = new System.Drawing.Point(125, 304);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(76, 56);
+            this.button7.TabIndex = 7;
+            this.button7.Text = " ";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.XOClick);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Silver;
+            this.button8.Location = new System.Drawing.Point(207, 304);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(76, 56);
+            this.button8.TabIndex = 8;
+            this.button8.Text = " ";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.XOClick);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Silver;
+            this.button9.Location = new System.Drawing.Point(289, 304);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(76, 56);
+            this.button9.TabIndex = 9;
+            this.button9.Text = " ";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.XOClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(526, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " Tik Tak Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ TikTakToe/TikTakToe/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TikTakToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void XOClick(object sender, EventArgs e)
+        {
+
+            Button bt = sender as Button;
+            //Label text içerisinde yazan karakter X ise buton texti X ile değiştiriyoruz ve tekrar basılamaması için etkin durumunu false yapıyoruz.Label texti ise O ile değiştiriyoruz.
+            if (label1.Text == "X")
+            {
+                bt.Text = "X";
+                bt.Enabled = false;
+                label1.Text = "O";
+            }
+            else//Label text X değilse diğer ihtimal olan O için yukardaki işlemlerin aynısını yapıyoruz.
+            {
+                bt.Text = "O";
+                bt.Enabled = false;
+                label1.Text = "X";
+            }
+
+
+            // X karakterinin kazanma ihtimalllerini sorguluyoruz.
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X")
+            {
+                MessageBox.Show("X KAZANDI.");
+                finish();
+            }
+            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+            {
+                MessageBox.Show("X KAZANDI.");
+                finish();
+            }
+            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("X KAZANDI.");
+                finish();
+            }
+            if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X")
+            {
+                MessageBox.Show("X KAZANDI.");
+                finish();
+            }
+            if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X")
+            {
+                MessageBox.Show("X KAZANDI."); 
+                finish();
+            }
+            if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("X KAZANDI.");
+                finish();
+            }
+            if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("X KAZANDI.");
+                finish();
+            }
+            if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                MessageBox.Show("X KAZANDI.");
+                finish();
+            }
+
+
+
+            // O karakteri kazanma ihtimallerini sorguluyoruz.
+
+            if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O")
+            {
+               MessageBox.Show("O KAZANDI.");
+                finish();
+            }
+            if (button4.Text == "O" && button5.Text == "O" && button6.Text == "O")
+            {
+                MessageBox.Show("O KAZANDI.");
+                finish();
+            }
+            if (button7.Text == "O" && button8.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("O KAZANDI.");
+                finish();
+            }
+            if (button1.Text == "O" && button4.Text == "O" && button7.Text == "O")
+            {
+                MessageBox.Show("O KAZANDI.");
+                finish();
+            }
+            if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
+            {
+                MessageBox.Show("O KAZANDI.");
+                finish();
+            }
+            if (button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("O KAZANDI.");
+                finish();
+            }
+            if (button1.Text == "O" && button5.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("O KAZANDI.");
+                finish();
+            }
+            if (button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                MessageBox.Show("O KAZANDI.");
+                finish();
+            }
+            //Bütün butonların texti dolu olduğunda oyun berabere kalmış olacaktır.Bu if sorgusunda da bunu sorguluyoruz.
+            if(button1.Text!="" 
+                && button2.Text != "" 
+                && button3.Text != "" 
+                && button4.Text != "" 
+                && button5.Text != ""
+                && button6.Text != "" 
+                && button7.Text != "" 
+                && button8.Text != "" 
+                && button9.Text != "")
+            {
+                MessageBox.Show("BERABERE");
+                finish();
+            }
+
+        }
+        //Finish addlı fonksiyonumuz ile oyunumuzu başlangıçdaki değerlerine getiriyoruz.
+        private void finish()
+        {
+            label1.Text = "X";
+            button1.Text = " ";
+            button2.Text = " ";
+            button3.Text = " ";
+            button4.Text = " ";
+            button5.Text = " ";
+            button6.Text = " ";
+            button7.Text = " ";
+            button8.Text = " ";
+            button9.Text = " ";
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+        }
+    }
+}
